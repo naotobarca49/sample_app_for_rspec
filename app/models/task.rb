5c7739a0ec62074c74000d5916490a50e64e3064
naotobarca49 <nnaoto49@icloud.com>
2020-11-03 05:39:53 +0900

    FactoryBotを使用してtaskのテストデータを作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: User, foreign_key: :user_id
   validates :title, presence: true, uniqueness: true
   validates :status, presence: true
   enum status: { todo: 0, doing: 1, done: 2 }
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title){ |n| "task#{n}" }
+    status :todo
+    association :owner
   end
 end
