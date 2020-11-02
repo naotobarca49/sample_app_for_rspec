2b054f1fd77eb5aa4711f3985c5c681489b0aa75
naotobarca49 <nnaoto49@icloud.com>
2020-11-03 07:54:32 +0900

    タイトルが無ければタスクが無効な状態であるテストの追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Task, type: :model do
       expect(task).to be_valid
       expect(task.errors).to be_empty
     end
+
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
   end
 end
