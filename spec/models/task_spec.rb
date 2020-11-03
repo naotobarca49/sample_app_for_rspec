4742a212e057bc5b9cc6ed3d400601307e449c04
naotobarca49 <nnaoto49@icloud.com>
2020-11-04 03:45:52 +0900

    Fix: リファクタリング

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      task = FactoryBot.build(:task)
+      task = build(:task)
       expect(task).to be_valid
       expect(task.errors).to be_empty
     end
