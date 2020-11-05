d2fc9fab4572d075cfd90fd64d953a06619762ed
naotobarca49 <nnaoto49@icloud.com>
2020-11-05 09:45:13 +0900

    フォームの入力値が正常な時、ログインに成功するテストの追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "UserSessions", type: :system do
+RSpec.describe "UserSessions", type: :system, focus: true do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログインに成功する' do
+        visit login_path
+        fill_in 'Email', with: user.email
+        fill_in 'password', with: 'password'
+        click_button 'Login'
+        expect(page).to have_content 'Login successful'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
