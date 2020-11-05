f9717b058409e1a123c4c21200811de837cac5c1
naotobarca49 <nnaoto49@icloud.com>
2020-11-05 09:00:16 +0900

    [解答参考] ログイン前のユーザー新規登録テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    describe 'ユーザー新規登録' do
+      context 'フォームの入力値が正常' do
+        it 'ユーザーの新規作成が成功する' do
+          visit new_user_path
+          fill_in 'Email', with: "email@example.com"
+          fill_in 'Password', with: "password"
+          fill_in 'Password confirmation', with: "password"
+          click_button "SignUp"
+          expect(page).to have_content 'User was successfully created.'
+          expect(current_path).to eq login_path
+        end
+      end
+
+      context 'メールアドレスが未入力' do
+        it 'ユーザーの新規作成が失敗する' do
+          visit new_user_path
+          fill_in 'Email', with: nil
+          fill_in 'Password', with: "password"
+          fill_in 'Password confirmation', with: "password"
+          click_button 'SignUp'
+          expect(page).to have_content '1 error prohibited this user from being saved'
+          expect(page).to have_content "Email can't be blank"
+          expect(current_path).to eq users_path
+        end
+      end
+    end
+  end
 end
