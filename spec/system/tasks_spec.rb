5316dab5efcb213209c0d2d41090d476ecf5978a
naotobarca49 <nnaoto49@icloud.com>
2020-11-05 14:36:53 +0900

    「ログイン後、タスク新規登録ページへのアクセスに成功する」テストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :system do
+RSpec.describe "Tasks", type: :system, focus: true do
   let(:user) { create(:user) }
   let(:task) { create(:task) }
 
@@ -10,7 +10,7 @@ RSpec.describe "Tasks", type: :system do
         it '新規登録ページのアクセスが失敗する' do
           visit new_task_path
           expect(page).to have_content 'Login required'
-          expect(current_path).to eq 'login_path'
+          expect(current_path).to eq login_path
         end
       end
 
@@ -18,7 +18,20 @@ RSpec.describe "Tasks", type: :system do
         it '編集ページのアクセスが失敗する' do
           visit edit_task_path(task)
           expect(page).to have_content 'Login required'
-          expect(current_path).to eq 'login_path'
+          expect(current_path).to eq login_path
+        end
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    before { login_as(user) }
+
+    describe 'ページ遷移確認' do
+      context 'タスクの新規登録ページにアクセス' do
+        it '新規登録ページのアクセスが成功する' do
+          click_link 'New task'
+          expect(current_path).to eq new_task_path
         end
       end
     end
