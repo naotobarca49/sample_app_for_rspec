0aa15c61eed328adb669e96ea3a21b70da41a844
naotobarca49 <nnaoto49@icloud.com>
2020-11-05 14:13:22 +0900

    [解答参考] 「ログイン前、タスクの新規登録ページのアクセスが失敗する」テストの追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
+  let(:user) { create(:user) }
+  let(:task) { create(:task) }
+
+  describe 'ログイン前' do
+    describe 'ページ遷移確認' do
+      context 'タスクの新規登録ページにアクセス' do
+        it '新規登録ページのアクセスが失敗する' do
+          visit new_task_path
+          expect(page).to have_content 'Login required'
+          expect(current_path).to eq 'login_path'
+        end
+      end
+    end
+  end
 end
