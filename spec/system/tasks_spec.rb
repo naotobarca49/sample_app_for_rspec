b4997a43e6c7192107fb7b6b5c59a28a298d6efa
naotobarca49 <nnaoto49@icloud.com>
2020-11-05 15:02:54 +0900

    「登録済のタイトル入力時、タスクの作成に失敗する」テストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -57,6 +57,21 @@ RSpec.describe "Tasks", type: :system, focus: true do
           expect(current_path). to eq tasks_path
         end
       end
+
+      context '登録済のタイトルを入力' do
+        it 'タスクの新規作成が失敗する' do
+          visit new_task_path
+          existed_task = create(:task)
+          fill_in 'Title', with: existed_task.title
+          fill_in 'Content', with: 'test_content'
+          select 'doing', from: 'Status'
+          fill_in 'Deadline', with: DateTime.new(2020, 6, 1, 10, 30)
+          click_button 'Create Task'
+          expect(page).to have_content '1 error prohibited this task from being saved:'
+          expect(page).to have_content "Title has already been taken"
+          expect(current_path). to eq tasks_path
+        end
+      end
     end    
   end
 end
